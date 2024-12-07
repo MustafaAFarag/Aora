9eb37734b3d461113f73dda8928d327e62d79741
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2024-12-07 22:50:15 +0200

    Creating User

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,71 @@
-import { View, Text } from "react-native";
-import React from "react";
+// @ts-nocheck
+
+import { View, Text, ScrollView, Image } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import FormField from "@/components/FormField";
+import CustomButtom from "@/components/CustomButtom";
+import { Link } from "expo-router";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  function handleSubmit() {}
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Text className="text-2xl text-white text-semibold font-psemibold mt-10">
+            Log in to Aora
+          </Text>
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStlyes="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStlyes="mt-7"
+            keyboardType="email-address"
+          />
+
+          <CustomButtom
+            title="Sign In"
+            handlePress={handleSubmit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have account?
+            </Text>
+            <Link
+              href="/sign-up"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Sign Up!
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,127 @@
-import { View, Text } from "react-native";
-import React from "react";
+// @ts-nocheck
+
+import { View, Text, ScrollView, Image, Alert } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import FormField from "@/components/FormField";
+import CustomButton from "@/components/CustomButtom";
+import { Link, router } from "expo-router";
+import { createUser } from "../../lib/appwrite";
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const validatePassword = (password) => {
+    const lengthValid = password.length >= 8 && password.length <= 265;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+
+    if (!lengthValid) {
+      throw new Error("Password must be between 8 and 265 characters.");
+    }
+    if (!(hasUpperCase && hasLowerCase && hasNumber)) {
+      throw new Error(
+        "Password must include uppercase, lowercase, a number, and a special character."
+      );
+    }
+  };
+
+  const isPasswordValid = (password) => {
+    try {
+      validatePassword(password);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
+
+  const handleSubmit = async () => {
+    if (!form.username || !form.email || !form.password) {
+      Alert.alert("Error", "Please fill in all the fields");
+      return;
+    }
+
+    if (!isPasswordValid(form.password)) {
+      Alert.alert(
+        "Error",
+        "Password is invalid. Ensure it meets the required format."
+      );
+      return;
+    }
+
+    setIsSubmitting(true);
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message || "Something went wrong");
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Text className="text-2xl text-white text-semibold font-psemibold mt-10">
+            Sign Up to Aora
+          </Text>
+          <FormField
+            title="Username"
+            value={form.username}
+            handleChangeText={(e) => setForm({ ...form, username: e })}
+            otherStlyes="mt-10"
+          />
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStlyes="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStlyes="mt-7"
+            secureTextEntry={true}
+          />
+
+          <CustomButton
+            title="Sign Up"
+            handlePress={handleSubmit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Have an account already?
+            </Text>
+            <Link
+              href="/sign-in"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Sign In!
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
